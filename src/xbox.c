b9595b16e7e473e5d39ddc6a8cb0edbaa69cd4b9
Zach Day <zachdayz@gmail.com>
2017-04-10 15:05:37 -0400

    Rewrote a lot of code. Now uses 32-bit depth for drawing window.

---
+++ src/xbox.c
@@ -0,0 +1,286 @@
+#include "xbox.h"
+
+struct xcb_context ctx;
+struct config config;
+
+void
+parse_config(int argc, char *argv[])
+{
+        /* Initialize default arguments. */
+        config.font = "Sans 30";
+        config.x = 30;
+        config.y = 30;
+        config.width = 200;
+        config.height = 50;
+        config.border = 5;
+        config.c_fg = (union rgba)(uint32_t)0xFFFFFFFFU;
+        config.c_bg = (union rgba)(uint32_t)0x00000000U;
+        config.c_border = (union rgba)(uint32_t)0x88888888U;
+        config.padding = 0;
+        config.align = PANGO_ALIGN_CENTER;
+        config.v_align = CENTER;
+
+        static int flag_help;
+        static int flag_c_border;
+
+        static struct option long_options[] = {
+                {"help", no_argument, &flag_help, 1},
+                {"font", required_argument, 0, 'f'},
+                {"x", required_argument, 0, 'x'},
+                {"y", required_argument, 0, 'y'},
+                {"width", required_argument, 0, 'w'},
+                {"height", required_argument, 0, 'h'},
+                {"border", required_argument, 0, 'b'},
+                {"fg-color", required_argument, 0, 't'},
+                {"bg-color", required_argument, 0, 'k'},
+                {"border-color", required_argument, &flag_c_border, 1},
+                {"padding", required_argument, 0, 'p'},
+                {"align", required_argument, 0, 'a'},
+                {"vertical-align", required_argument, 0, 'v'},
+                {0, 0, 0, 0}
+        };
+
+        /* Set to given arguments or print information. */
+        int opt;
+        int long_index;
+        while ((opt = getopt_long(argc, argv, ":f:x:y:w:h:b:t:k:p:a:v:", long_options, &long_index)) != -1) {
+                if (flag_help == 1) {
+                        printf(USAGE);
+                        exit(EXIT_SUCCESS);
+                }
+
+                if (flag_c_border == 1) {
+                        printf("Setting border color.\n");
+                        config.c_border = parse_color_string(optarg);
+                }
+
+                switch (opt) {
+                        case 'f':
+                                config.font = optarg;
+                                break;
+                        case 'x':
+                                config.x = atoi(optarg);
+                                break;
+                        case 'y':
+                                config.y = atoi(optarg);
+                                break;
+                        case 'w':
+                                config.width = atoi(optarg);
+                                break;
+                        case 'h':
+                                config.height = atoi(optarg);
+                                break;
+                        case 'b':
+                                config.border = atoi(optarg);
+                                break;
+                        case 't':
+                                config.c_fg = parse_color_string(optarg);
+                                break;
+                        case 'k':
+                                config.c_bg = parse_color_string(optarg);
+                                break;
+                        case 'p':
+                                config.padding = atoi(optarg);
+                                break;
+                        case 'a':
+                                if (*optarg == 'l') {
+                                        config.align = PANGO_ALIGN_LEFT;
+                                } else if (*optarg == 'c') {
+                                        config.align = PANGO_ALIGN_CENTER;
+                                } else if (*optarg == 'r') {
+                                        config.align = PANGO_ALIGN_RIGHT;
+                                } else {
+                                        printf("Unrecognized alignment setting.\n");
+                                        exit(EXIT_FAILURE);
+                                }
+                                break;
+                        case 'v':
+                                if (*optarg == 't') {
+                                        config.v_align = TOP;
+                                } else if (*optarg == 'c') {
+                                        config.v_align = CENTER;
+                                } else if (*optarg == 'b') {
+                                        config.v_align = BOTTOM;
+                                } else {
+                                        printf("Unrecognized alignment setting.\n");
+                                        exit(EXIT_FAILURE);
+                                }
+                                break;
+                        case ':':
+                                printf("Option requires an argument.\n");
+                                exit(EXIT_FAILURE);
+                }
+        }
+}
+
+union rgba
+parse_color_string(const char *c)
+{
+        /* Thanks lemonboy. */
+        union rgba temp = (union rgba)(uint32_t)strtoul(c, NULL, 16);
+        return (union rgba){
+                .r = (temp.r * temp.a) / 255,
+                .g = (temp.g * temp.a) / 255,
+                .b = (temp.b * temp.a) / 255,
+                .a = temp.a
+        };
+}
+
+/* Attempts to find an RGBA visual. */
+xcb_visualtype_t*
+get_visualtype(void)
+{
+        xcb_depth_iterator_t depth_iter = xcb_screen_allowed_depths_iterator(ctx.screen);
+        xcb_visualtype_iterator_t visual_iter;
+
+        if (depth_iter.data) {
+                for (; depth_iter.rem; xcb_depth_next(&depth_iter)) {
+                        if (depth_iter.data->depth == 32) {
+                                for (visual_iter = xcb_depth_visuals_iterator(depth_iter.data); visual_iter.rem; xcb_visualtype_next(&visual_iter)) {
+                                        return visual_iter.data;
+                                }
+                        }
+                }
+        }
+
+        return NULL;
+}
+
+/* Sets up the primary xcb_context container. */
+void
+initialize_context(void)
+{
+        /* XCB */
+        ctx.conn = xcb_connect(NULL, NULL);
+
+        ctx.screen = xcb_setup_roots_iterator(xcb_get_setup(ctx.conn)).data;
+
+        ctx.visual = get_visualtype();
+
+        ctx.window = xcb_generate_id(ctx.conn);
+
+        ctx.colormap = xcb_generate_id(ctx.conn);
+        xcb_create_colormap(ctx.conn, XCB_COLORMAP_ALLOC_NONE, ctx.colormap, ctx.screen->root, ctx.visual->visual_id);
+
+        /* Cairo and Pango */
+        ctx.surface = cairo_xcb_surface_create(ctx.conn, ctx.window, ctx.visual, config.width, config.height);
+        ctx.cr = cairo_create(ctx.surface);
+        ctx.layout = pango_cairo_create_layout(ctx.cr);
+}
+
+void
+create_window(void)
+{
+        initialize_context();
+
+        uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_BORDER_PIXEL | XCB_CW_OVERRIDE_REDIRECT | XCB_CW_EVENT_MASK | XCB_CW_COLORMAP;
+        const uint32_t values[5] = {
+                config.c_bg.val,
+                config.c_border.val,
+                1,
+                XCB_EVENT_MASK_EXPOSURE,
+                ctx.colormap
+        };
+
+        int depth = (ctx.visual->visual_id == ctx.screen->root_visual) ? XCB_COPY_FROM_PARENT : 32;
+        printf("Using a %d-bit depth for drawing.\n", depth);
+        xcb_create_window(ctx.conn, depth,
+                        ctx.window, ctx.screen->root,
+                        config.x, config.y, config.width, config.height, config.border,
+                        XCB_WINDOW_CLASS_INPUT_OUTPUT,
+                        ctx.visual->visual_id,
+                        mask, values);
+
+        xcb_change_property(ctx.conn,
+                        XCB_PROP_MODE_REPLACE,
+                        ctx.window,
+                        XCB_ATOM_WM_NAME,
+                        XCB_ATOM_STRING,
+                        8,
+                        strlen("xbox"),
+                        "xbox");
+
+        xcb_change_property(ctx.conn,
+                        XCB_PROP_MODE_REPLACE,
+                        ctx.window,
+                        XCB_ATOM_WM_CLASS,
+                        XCB_ATOM_STRING,
+                        8,
+                        strlen("xbox"),
+                        "xbox");
+
+        xcb_free_colormap(ctx.conn, ctx.colormap);
+}
+
+void
+event_loop(void)
+{
+        xcb_generic_event_t *event;
+        while ((event = xcb_wait_for_event(ctx.conn)) != NULL) {
+                free(event);
+                xcb_flush(ctx.conn);
+        }
+}
+
+void
+draw_text(const char *text)
+{
+        /* Initialize the Pango layout. */
+        pango_layout_set_width(ctx.layout, config.width * PANGO_SCALE);
+        pango_layout_set_height(ctx.layout, config.height * PANGO_SCALE);
+
+        /* Account for padding. */
+        pango_layout_set_width(ctx.layout, pango_layout_get_width(ctx.layout) - config.padding * 2 * PANGO_SCALE);
+
+        /* Set the text font. */
+        pango_layout_set_text(ctx.layout, text, -1);
+        PangoFontDescription *desc = pango_font_description_from_string(config.font);
+        pango_layout_set_font_description(ctx.layout, desc);
+        pango_font_description_free(desc);
+
+        /* Vertically center the text. */
+        int height;
+        pango_layout_get_pixel_size(ctx.layout, NULL, &height);
+        cairo_translate(ctx.cr, config.padding, 0);
+        if (config.v_align == TOP) {
+                cairo_translate(ctx.cr, 0, config.padding);
+        } else if (config.v_align == CENTER) {
+                cairo_translate(ctx.cr, 0, (double) config.height / 2. - (double) height / 2.);
+        } else {
+                cairo_translate(ctx.cr, 0, (double) config.height - config.padding - height);
+        }
+
+        cairo_translate(ctx.cr, config.padding, 0);
+
+        /* Draw the text. */
+        cairo_set_source_rgba(ctx.cr, (double) config.c_fg.r / 255.0, (double) config.c_fg.g / 255.0, (double) config.c_fg.b / 255.0, (double) config.c_fg.a / 255.0);
+        pango_cairo_show_layout(ctx.cr, ctx.layout);
+
+        cairo_surface_flush(ctx.surface);
+}
+
+int
+main(int argc, char *argv[])
+{
+        parse_config(argc, argv);
+
+        char text[1024];
+        int len = read(0, text, 1024);
+        if (text[len - 1] == '\n') {
+                text[len - 1] = '\0';
+        } else {
+                text[len] = '\0';
+        }
+
+        initialize_context();
+
+        create_window();
+
+        xcb_map_window(ctx.conn, ctx.window);
+        draw_text(text);
+        xcb_flush(ctx.conn);
+
+        event_loop();
+        xcb_disconnect(ctx.conn);
+        return 0;
+}
---
+++ src/xbox.h
@@ -0,0 +1,87 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <xcb/xcb.h>
+#include <cairo/cairo-xcb.h>
+#include <pango-1.0/pango/pangocairo.h>
+
+#define USAGE "Usage: xbox [OPTIONS]\n"\
+"   --help              Show this help\n"\
+"-f --font [FONT]       Print text with font FONT\n"\
+"-x --xpos [X]          Set the x-coordinate of the box\n"\
+"-y --ypos [Y]          Set the y-coordinate of the box\n"\
+"-w --width [WIDTH]     Set the width of the box\n"\
+"-h --height [HEIGHT]   Set the height of the box\n"\
+"-b --border [BORDER]   Set the border width of the box\n"\
+"-t --fg-color [COLOR]  Draw text with the color COLOR\n"\
+"-k --bg-color [COLOR]  Draw box with the background color COLOR\n"\
+"-a --align (l|c|r)     Horizontally align text left, center, or right\n"\
+"-p --padding [PADDING] Horizontally pad the text by PADDING pixels\n"\
+"-v --vertical-align (t|c|b)\n"\
+"                       Vertically align text top, center, or bottom\n"\
+"\nColors should be given in the form RRGGBB.\n"\
+"All measurements are in pixels.\n"
+
+/* An RGBA color map represented as a 32-bit integer. */
+union rgba {
+        struct {
+                uint8_t b;
+                uint8_t g;
+                uint8_t r;
+                uint8_t a;
+        };
+        uint32_t val;
+};
+
+typedef enum vertical_align {
+        TOP,
+        CENTER,
+        BOTTOM
+} vertical_align;
+
+/* Data necessary for interacting with the X server. */
+struct xcb_context {
+        xcb_connection_t *conn;
+        xcb_screen_t *screen;
+        xcb_visualtype_t *visual;
+        xcb_window_t window;
+        xcb_colormap_t colormap;
+
+        cairo_t *cr;
+        cairo_surface_t *surface;
+        PangoLayout *layout;
+};
+
+/* Given command-line arguments. */
+struct config {
+        const char *font;
+        uint32_t x, y, width, height;
+        uint32_t border;
+        uint32_t padding;
+        union rgba c_fg, c_bg, c_border;
+        PangoAlignment align;
+        vertical_align v_align;
+};
+
+void
+parse_config(int argc, char *argv[]);
+
+union rgba
+parse_color_string(const char *c);
+
+xcb_visualtype_t*
+get_visualtype(void);
+
+void
+initialize_context(void);
+
+void
+create_window(void);
+
+void
+event_loop(void);
+
+void
+draw_text(const char *text);
