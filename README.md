9aaa82899116ad31bc29c8b4d2604bb6e2355ff6
Zacharie Day <zachdayz@gmail.com>
2018-01-29 12:51:22 -0500

    Update README and clean up documentation

--- README.md
+++ README.md
@@ -1,14 +1,52 @@
 box11
 =====
 
-`box11` is the next generation in box-drawing technology. Created as an exercise in programming for the X window server, `box11` provides a simple way to display given text anywhere on the screen.
+## Synopsis
+
+`box11` is the next generation in box-drawing technology. It provides a simple
+way to display text anywhere on the screen.
+
+Run `box11 --help` for usage information.
 
 ## Usage
-`box11` displays standard input.
 
-## Building
-`xcb`, `xcb-xrm`, `cairo`, and `pango` are required to build `box11`. Consult your distribution's repositories for package names.
+`box11` displays standard input. For example:
+
+```sh
+while true; do 
+    date
+    sleep 1
+done | box11 -u hv
+```
+
+This will write the date in a grey box in the upper-left corner of the screen,
+updating it once per second. The box will automatically resize to fit the input
+text.
+
+## Build
+
+### Dependencies
+
+`xcb`, `xcb-xrm`, `cairo`, and `pango` are required to build `box11`. Consult
+your distribution's repositories for package names.
 
 ### Steps
+
+To build, run:
+
 * `mkdir build`
 * `make`
+
+This will place the `box11` executable in the `build` directory. 
+
+To install it system-wide, simply `make install`. Running that command with the
+`INSTALL_PATH` environment variable set will override the directory to which
+the `box11` executable is installed.
+
+## Future goals
+
+This project could be expanded in a number of ways:
+
+* Image rendering support
+* Come up with a better name (classic UNIX commands tend not to have numbers in executable names)
+* User input (probably better suited to another project like `dmenu`)
--- src/box11.h
+++ src/box11.h
@@ -6,23 +6,23 @@
 #define APP_NAME "box11"
 
 #define USAGE \
-"Usage: box11 [OPTIONS]\n"\
-"   --help              Show this help\n"\
-"-f --font [FONT]       Print text with font FONT\n"\
-"-u --autosize (h)(v)   Adjust the window dimensions to fit the text. Overrides other positioning arguments\n"\
-"-x --xpos [X]          Set the x-coordinate of the box\n"\
-"-y --ypos [Y]          Set the y-coordinate of the box\n"\
-"-w --width [WIDTH]     Set the width of the box\n"\
-"-h --height [HEIGHT]   Set the height of the box\n"\
-"-b --border [BORDER]   Set the border width of the box\n"\
-"-t --fg-color [COLOR]  Draw text with the color COLOR\n"\
-"-k --bg-color [COLOR]  Draw box with the background color COLOR\n"\
-"-a --align (l|c|r)     Horizontally align text left, center, or right\n"\
-"-p --padding [PADDING] Horizontally pad the text by PADDING pixels\n"\
-"-v --vertical-align (t|c|b)\n"\
-"                       Vertically align text top, center, or bottom\n"\
-"\nColors should be given in the form AARRGGBB.\n"\
-"All measurements are in pixels.\n"
+    "Usage: box11 [OPTIONS]\n"\
+    "   --help              Show this help\n"\
+    "-f --font [FONT]       Print text with font FONT\n"\
+    "-u --autosize (h)(v)   Adjust the window dimensions to fit the text. Overrides other positioning arguments\n"\
+    "-x --xpos [X]          Set the x-coordinate of the box\n"\
+    "-y --ypos [Y]          Set the y-coordinate of the box\n"\
+    "-w --width [WIDTH]     Set the width of the box\n"\
+    "-h --height [HEIGHT]   Set the height of the box\n"\
+    "-b --border [BORDER]   Set the border width of the box\n"\
+    "-t --fg-color [COLOR]  Draw text with the color COLOR\n"\
+    "-k --bg-color [COLOR]  Draw box with the background color COLOR\n"\
+    "-a --align (l|c|r)     Horizontally align text left, center, or right\n"\
+    "-p --padding [PADDING] Horizontally pad the text by PADDING pixels\n"\
+    "-v --vertical-align (t|c|b)\n"\
+    "                       Vertically align text top, center, or bottom\n"\
+    "\nColors should be given in the form AARRGGBB.\n"\
+    "All measurements are in pixels.\n"
 
 #define DPI_SCALE_DIVISOR 96
 
